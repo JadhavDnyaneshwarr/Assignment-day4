cbd7a866fd369728c280140f40cb86a421ce086d
JadhavDnyaneshwarr <118913484+JadhavDnyaneshwarr@users.noreply.github.com>
2022-11-29 19:45:52 +0530

    all the problems of if-else ladder solution

---
+++ day_date_from_20march_to_20june.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter the date: " date;
+read -p "Enter the month: " month;
+
+if [ $month -ge 3 ] && [ $date -ge 20 ] && [ $date -le 31 ]
+then
+	echo "True";
+
+elif [ $month -le 6 ] && [ $date -le 20 ]
+then
+	echo "True";
+else
+	echo "False";
+fi
